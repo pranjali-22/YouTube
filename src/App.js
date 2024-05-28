9447c4ec9b8f8ba2ee5cb1d4107293d641a87f48
pdas05 <pdas05@students.cs.ubc.ca>
2024-05-28 00:36:20 -0700

    Uploading files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import {
+  ChannelDetails,
+  Feed,
+  SearchFeed,
+  Sidebar,
+  VideoDetails,
+} from "./pages/index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { red } from "@mui/material/colors";
+// import Stack from "@mui/material/Stack";
+// import LinearProgress from "@mui/material/LinearProgress";
+// import { createTheme } from "@mui/material/styles";
+// import { useSelector } from "react-redux";
 
-const App = () => {
+function App() {
   return (
-    <div className = "text-3xl">App</div>
-  )
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/feed/:id" element={<Feed />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+        <Route path="/search/:id" element={<SearchFeed />} />
+        <Route path="/watch/:id" element={<VideoDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
